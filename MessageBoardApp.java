f67bb5a836f45ed4d0440bc81667d8c036946973
DevilGlitch <73667748+DevilGlitch@users.noreply.github.com>
2023-03-07 18:34:56 -0800

    Main App

---
+++ MessageBoardApp.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MessageBoardApp extends HttpServlet {
+    private MessageBoard messageBoard;
+
+    public void init() throws ServletException {
+        messageBoard = new MessageBoard();
+        getServletContext().setAttribute("messageBoard", messageBoard);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("message_board.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
